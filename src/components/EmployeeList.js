6c76b69970d3a4e2dc6e90b63ef08a560705eada
Matt <mattysmith@gmail.com>
2018-07-29 09:58:35 +1000

    feat(employees) Add chips
    Add chips for filtering

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -4,26 +4,39 @@ import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
+import Chip from '@material-ui/core/Chip';
 import EmployeeCard from './EmployeeCard'
 
 const styles = theme => ({
-  heading: {
+  container: {
     padding: theme.spacing.unit * 5
+  },
+  header: {
+    display: 'flex',
+    'justify-content': 'space-between'
   },
   list: {
     marginTop: theme.spacing.unit * 5
+  },
+  chip: {
+    margin: theme.spacing.unit,
   }
 });
 
 const EmployeeList = props => {
   const { classes, employees } = props;
   return (
-    <div className={classes.heading}>
-      <Typography variant="display1">
-        Our Employees
-      </Typography>
+    <div className={classes.container}>
+      <div className={classes.header}>
+        <Typography variant="display1">
+          Our Employees
+        </Typography>
+        <div>
+          <Chip label="First name" className={classes.chip}/>
+          <Chip label="Last name" className={classes.chip}/>
+        </div>
+      </div>
       <Divider />
-  
       <Grid container spacing={24} className={classes.list}>
         {employees.map(e => (
           <Grid item xs={12} sm={6} lg={4} xl={3} key={e.id}>
